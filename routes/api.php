3feeec103703d90749019eb68ab0ab498ff501ad
Elon Zito <simsketch@gmail.com>
2023-06-01 12:02:31 -0400

    Add get, post, and data migration

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Models\Bird;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +17,15 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::get('/birds', function () {
+    return Bird::all();
+});
+
+Route::post('/birds', function () {
+    return Bird::create([
+        'name' => request('name'),
+        'description' => request('description'),
+    ]);
 });
